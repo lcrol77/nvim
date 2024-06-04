c0c2715295c3daa14e29852cfe16aa4cc4c87a55
lcroley <logan.croley.18@gmail.com>
2024-06-04 15:18:04 -0400

    feat: added references and doc symbols to telescope hotkeys

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set("n", "<leader>vrr", builtin.lsp_references)
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)

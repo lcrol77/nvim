bd5cc833446755e24638fbf5bbab884ee9563243
Logan Croley <logan.croley@wellhive.com>
2025-10-30 12:53:10 -0400

    added go to impl

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set("n", "<leader>vrr", builtin.lsp_references)
+vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, { desc = "Go to implementation" })
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)

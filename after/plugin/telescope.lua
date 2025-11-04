86597517f804544f0b1d9f40f8f6f52724bed0fc
lcroley <logan.croley@wellhive.com>
2025-11-04 14:21:21 -0500

    file specific grep command

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,6 +4,15 @@ vim.keymap.set('n', '<leader>pg', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set("n", "<leader>vrr", builtin.lsp_references)
 vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, { desc = "Go to implementation" })
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>pa', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>ps', function()
+  local word = vim.fn.input("Grep > ")
+  local file = vim.fn.expand("%:p")
+  require('telescope.builtin').grep_string({
+    search = word,
+    search_dirs = { file },
+  })
+end)
+
